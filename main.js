4672d553d4a51aa4423e03231a153a87471390f8
TayyabAziz11 <159780590+TayyabAziz11@users.noreply.github.com>
2024-03-12 15:48:24 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+// Print the quote and the name of its author
+var quote = "Accept the apology even if it is not sincere";
+var author = "Hazrat Aliرضی اللہ عنہ";
+console.log("".concat(author, ",once said: ").concat(quote));
---
+++ main.ts
@@ -0,0 +1,7 @@
+// Print the quote and the name of its author
+
+let quote: string = "Accept the apology even if it is not sincere";
+
+let author: string = "Hazrat Aliرضی اللہ عنہ";
+
+console.log(`${author},once said: ${quote}`);
